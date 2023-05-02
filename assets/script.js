dd289f6c8deeeaf961a6d543e7994b20b75bf678
DKsolomon <DKSolomon8135@gmail.com>
2023-05-01 22:11:16 -0700

    added 5 day forecast functions

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 var APIkey = '116630283f8adda1965e22975fef6733';
-var cityInput = document.getElementById("city-searched");
+var cityInput = document.getElementById("city-input");
 var submitBtn = document.getElementById("submitbtn");
 var pastCity = document.getElementById("past-cities");
 var searchedCities = [];
 var currentForecast = document.getElementById("current-forecast");
-var currentCityEl = document.getElementById("city-info");
+var currentCityInfo = document.getElementById("city-info");
 var currentConditions = document.getElementById("current-conditions");
 var fiveDayForecast = document.getElementById("5-day-forecast");
 
-function getWeather() {
+function grabCurrentWeather() {
   
     var city = cityInput.value;
     var currentDay = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + APIkey;
@@ -21,30 +21,86 @@ function getWeather() {
       })
       .then(function (data) {
         console.log(data);
-        displayResults(data);
+        displayCurrentWeather(data);
+        grabFiveDayForecast(data);
       }); 
       
      };
 
-     function displayResults(data) {
+     function displayCurrentWeather(data) {
 
         var cityName = data.city.name;
         var weatherCondition = data.list[0].weather[0].main;
         var weatherDescription = data.list[0].weather[0].icon;
         var temperature = data.list[0].main.temp;
         var windspeed = data.list[0].wind.speed;
-        var humidity = data.list[0].main.humidity
+        var humidity = data.list[0].main.humidity;
         var date = new Date().toLocaleDateString();
         var iconLink ='http://openweathermap.org/img/wn/' + weatherDescription + '@2x.png';
      
-        currentCityEl.innerHTML = '<h2>' + cityName + ' '  + date  + "<img src ='"+iconLink+"'/>";
-        currentConditions.innerHTML = '</h2>' + '<p>Weather:'+ weatherCondition  + '</p>' +'<p>Temperature:' + temperature + '°F</p>' + '<p>Windspeed: ' + windspeed + 'mph</p>' + '<p>Humidity: ' + humidity + '%</p>';
+        currentCityInfo.innerHTML = '<h2>' + cityName + ' '  + date  + "<img src ='"+iconLink+"'/>";
+        
+        currentConditions.innerHTML = '</h2>' + '<p>Weather:'+ weatherCondition  + '</p>' +
+        '<p>Temperature:' + temperature + '°F</p>' + '<p>Windspeed: ' + windspeed + 'mph</p>' + '<p>Humidity: ' + humidity + '%</p>';
      
         
-         };
+         }; 
+
+         function grabFiveDayForecast(data) {
+      
+            console.log(data);
+            var forecastList = data.list;
+            console.log(forecastList);
+            var fiveDayData = [];
+            for (var i = 0; i < forecastList.length; i += 8) {
+  
+              var dateAndTime = forecastList[i].dt_txt;
+              var dateObj = new Date(dateAndTime);
+              var month = dateObj.getMonth() + 1;  
+              var day = dateObj.getDate();
+              var year = dateObj.getFullYear();
+              var date = month + '/' + day + '/' + year;
+  
+              var temperature = forecastList[i].main.temp;
+              var weatherDescription = forecastList[i].weather[0].description;
+              var windSpeed = forecastList[i].wind.speed;
+              var humidity = forecastList[i].main.humidity;
+              var icon = forecastList[i].weather[0].icon;
+              fiveDayData.push({ date, temperature, weatherDescription, windSpeed, humidity, icon});
+            }
+            
+            displayFiveDayForecast(fiveDayData);
+         
+      }
+   
+      function displayFiveDayForecast(fiveDayData) {
+        
+        fiveDayForecast.innerHTML = '';
+        
+        for (var i = 0; i < fiveDayData.length; i++) {
+          var date = fiveDayData[i].date;
+          var temperature = fiveDayData[i].temperature;
+          var weatherDescription = fiveDayData[i].weatherDescription;
+          var windSpeed = fiveDayData[i].windSpeed;
+          var humidity = fiveDayData[i].humidity;
+          var icon = fiveDayData[i].icon;
+          var iconLink ='http://openweathermap.org/img/wn/' + icon + '@2x.png';
+  
+          var dayEl = document.createElement('div');
+          dayEl.classList = ('card-body');
+          
+          dayEl.innerHTML =  '<h3>' + date + '</h3>' + '<p>Temperature: ' + temperature +
+           '°F</p>' + '<p>Weather: ' + weatherDescription + "<img src ='"+iconLink+"'/>" +
+             '</p>' + '<p>Wind Speed: ' + windSpeed + 'mph</p>' + '<p>Humidity: ' + humidity + '%</p>';
+      
+          
+          fiveDayForecast.appendChild(dayEl);
+          
+        }
+      };
 
          submitBtn.addEventListener('click', function () {
-           getWeather();
+           grabCurrentWeather();
             searchedCities.push(cityInput.value);
             localStorage.setItem('searchedCities', JSON.stringify(searchedCities));
           });
